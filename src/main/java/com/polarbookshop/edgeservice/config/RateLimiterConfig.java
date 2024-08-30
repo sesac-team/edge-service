eeda347807b5ca7136138935dda9a5a7da9d3d27
gmdcjdakdcjd <ysh010223@naver.com>
2024-08-30 13:19:38 +0900

    remove secu

--- src/main/java/com/polarbookshop/edgeservice/config/RateLimiterConfig.java
+++ src/main/java/com/polarbookshop/edgeservice/config/RateLimiterConfig.java
@@ -1,19 +1,17 @@
 package com.polarbookshop.edgeservice.config;
 
-import java.security.Principal;
-
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class RateLimiterConfig {
 
 	@Bean
 	KeyResolver keyResolver() {
-		return exchange -> exchange.getPrincipal()
-				.map(Principal::getName)
-				.defaultIfEmpty("anonymous");
+		// 모든 요청에 대해 동일한 키("anonymous")를 사용하여 레이트 리미팅을 적용
+		return exchange -> Mono.just("anonymous");
 	}
 
 }
